31625efd312815ebc0315e698b9876047a24fe08
Aruna Turay <tura_arun@yahoo.com>
2023-08-08 00:30:01 -0400

    all passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", "3", "M", "brown", "friendly", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", "5", "M", "black", "aggressive", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", "7", "F", "brown", "angry", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", "10", "M", "brown", "bossy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", "6", "F", "black", "shy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", "11", "M", "black", "grumpy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", "12", "F", "brown", "playful", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, "5", "M", "black", "sad", "1");
